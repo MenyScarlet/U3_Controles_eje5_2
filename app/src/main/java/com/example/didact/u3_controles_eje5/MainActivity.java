ed615ca49af475375c8d1c7afad8137c0a352712
MenyScarlet <melania.gallego.22@gmail.com>
2018-02-08 00:37:06 +0100

    diseño y objetos declarados

--- app/src/main/java/com/example/didact/u3_controles_eje5/MainActivity.java
+++ app/src/main/java/com/example/didact/u3_controles_eje5/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.didact.u3_controles_eje5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNombre, etApellidos, etEdad;
+    Spinner spProvincia;
+    RadioGroup rgEstadoCivil;
+    CheckBox cbTerminos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        etNombre = (EditText)findViewById(R.id.etNombre);
+        etApellidos = (EditText)findViewById(R.id.etApellidos);
+        etEdad = (EditText)findViewById(R.id.etEdad);
+        spProvincia = (Spinner)findViewById(R.id.spProvincias);
+        rgEstadoCivil = (RadioGroup)findViewById(R.id.rgEstadoCivil);
+        cbTerminos = (CheckBox)findViewById(R.id.cbTerminos);
+
+        String[] provincias =
+                {"Cádiz", "Sevilla","Huelva","Málaga", "Córdoba","Granada","Jaén", "Almeria"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, provincias);
+        spProvincia.setAdapter(adaptador);
+
+
+
+    }//FIN onCreate
+
+
+
+}//FIN MainActivity
